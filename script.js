5ef4587e5a2c71e0764043fea4e803b8834164d7
victordev86 <victorvyg@me.com>
2023-06-07 10:04:23 -0300

    started Javascript aplication on Menu (mobile version)

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuMobile = document.querySelector(".menu-mobile")
+
+function menuClick() {
+
+    
+    console.log("botao clicado")
+}
+
+menuMobile.addEventListener("click", menuClick)
+
+console.log(menuMobile)
